a109b8c2677047bb4cdb839e79faa7f76965c70e
kymera_28 <tobiasponce28@gmail.com>
2016-04-13 22:44:14 -0300

    first.php updates

--- first.php
+++ first.php
@@ -1,30 +1,33 @@
 <?php
 	session_start();
-	if ($_SESSION['showMenu'] != true) {
+	if (!isset($_SESSION['showMenu'])) {
+		header('Location: index.php');
+	}elseif ($_SESSION['showMenu'] != true) {
 		echo 'sem menu';
-		
-	}
+	}else{
 ?>
-<html>
-<head>
-	<title>Login</title>
-	<meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="style.css">
-<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
-</head>
-<body>
-<header>
-	<nav>
-		<ul>
-			<li><a href="">Inicio</a></li>
-			<li><a href="">Cadastrar</a></li>
-			<li><a href="">Usuários</a></li>
-			<li><i class="fa fa-sign-out" aria-hidden="true"></i><a href="logout.php"> Sair</a></li>
-		</ul>
-	</nav>
-</header>
+
+	<html>
+	<head>
+		<title>Login</title>
+		<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+	</head>
+	<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="">Inicio</a></li>
+				<li><a href="">Cadastrar</a></li>
+				<li><a href="">Usuários</a></li>
+				<li><i class="fa fa-sign-out" aria-hidden="true"></i><a href="logout.php"> Sair</a></li>
+			</ul>
+		</nav>
+	</header>
 
 
-</body>
-</html>
+	</body>
+	</html>
+<?php } ?>
