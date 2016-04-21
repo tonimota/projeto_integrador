e4376df388459fcab9039fca35b5abd7cb067aaa
kymera_28 <tobiasponce28@gmail.com>
2016-04-20 21:53:00 -0300

    teste

--- first.php
+++ first.php
@@ -3,6 +3,18 @@
 	if (!isset($_SESSION['showMenu'])) {
 		header('Location: index.php');
 	}
+
+	include 'db.php';
+
+	//CONSULTA DB
+	$stmt = odbc_prepare($conn, "select * from Professor");
+
+    odbc_execute($stmt);
+    echo odbc_errormsg($conn);
+   	//print_r(odbc_fetch_array($stmt));
+   	$result = odbc_fetch_array($stmt);
+
+    $rows = odbc_num_rows($stmt);
 ?>
 
 	<html>
@@ -14,21 +26,51 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 	</head>
 	<body>
-	<header>
-		<?php if ($_SESSION['showMenu'] == true) {?>
-		<nav>
-			<ul>
-				<li><a href="">Inicio</a></li>
-				<li><a href="">Cadastrar</a></li>
-				<li><a href="">Usuários</a></li>
-				<li><i class="fa fa-sign-out" aria-hidden="true"></i><a href="logout.php"> Sair</a></li>
-			</ul>
-		</nav>
-		<?php }else{
-			echo 'sem menu';
-		}?>
-	</header>
+		<header>
+			<?php if ($_SESSION['showMenu'] == true) {?>
+			<nav>
+				<ul>
+					<li><a href="">Inicio</a></li>
+					<li><a href="">Cadastrar</a></li>
+					<li><a href="">Usuários</a></li>
+					<li><i class="fa fa-sign-out" aria-hidden="true"></i><a href="logout.php"> Sair</a></li>
+				</ul>
+			</nav>
+			<?php }else{
+				echo 'sem menu';
+			}?>
+		</header>
+		<div id='wrapper'>
+			<div class='table-holder'>
+				<table>
+					<tr>
+						<th>Cod. Usuario</th>
+						<th>Nome</th>
+						<th>E-mail</th>
+						<th>Id Senac</th>
+						<th>Tipo</th>
+					</tr>
 
+					<?php foreach ($result as $key=>$value) {
+						print_r($value);
+						print_r($key);
+
+						?>	
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+					<?php }?>
+				</table>
+			</div>
+		</div>
+
+		<footer>
+			
+		</footer>
 
 	</body>
 	</html>
